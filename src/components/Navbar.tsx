e25b76b197debce3ecf28fe913ecc9d39e249e0d
John Peter Nuwagaba <95671518+jpnuwagaba@users.noreply.github.com>
2025-03-13 18:44:37 +0300

    Update Navbar.tsx new logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
         >
           <Link href={"/"}>
             <Image
-              src="/assets/logo.png"
+              src="/assets/scapes logo.png"
               width={100}
               height={100}
               className="w-[90px] md:w-24"
